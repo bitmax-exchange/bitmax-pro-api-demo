b9f9d196159be1fe476cf4950a99d75848d5d49b
yuez-gdm <yue.zhao@gdmexchange.io>
2019-12-09 16:36:32 -0500

    changeFileName

--- python/query_pub_barhist.py
+++ python/query_pub_barhist.py
@@ -1,4 +1,4 @@
-import os
+import os 
 import click
 import requests
 from pprint import pprint
@@ -10,21 +10,31 @@ from util import *
 @click.command()
 @click.option("--symbol", type=str, default="BTMX/USDT")
 @click.option("--interval", type=str, default="1")
+@click.option("--frm", type=int)
+@click.option("--to", type=int)
 @click.option("--n", type=int, default=10)
-@click.option("--frm", type=int, default=None)
-@click.option("--to", type=int, default=None)
 @click.option("--config", type=str, default="config.json")
-def run(symbol, interval, n, frm, to, config):
+def run(symbol, interval, frm, to, n, config):
+    if config is None:
+        config = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "config.json")
+        print(f"Config file is not specified, use {config}")
+    btmxCfg = load_config(config)['bitmax']
 
-    btmx_cfg = load_config(get_config_or_default(config))['bitmax']
-
-    host = btmx_cfg['https']
+    host = btmxCfg['https']
+  
     url = f"{host}/api/pro/barhist"
-    params = {"symbol": symbol, "interval": interval, "n": n, "from": frm, "to": to}
+    params = {
+        "symbol":   symbol,
+        "interval": interval,
+        "n":        n,
+        "from":     frm,
+        "to":       to,
+    }
 
-    res = requests.get(url, params=params)
+    print(url)
+    res = requests.get(url, params = params)
     pprint(parse_response(res))
 
 
-if __name__ == "__main__":
-    run()
+if __name__ == "__main__": 
+    run()  
