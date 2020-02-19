1918dff8a252aad7b5111888832a2f3b523f61f0
yuez-gdm <yue.zhao@gdmexchange.io>
2020-02-19 12:50:31 -0500

    +showAllFilterForBalance

--- python/query_prv_balance.py
+++ python/query_prv_balance.py
@@ -10,10 +10,11 @@ from util import *
 @click.command()
 @click.option("--asset", type=str, default=None,
               help='optional, if none, return all assets with non-empty balance. You can specify an asset (e.g. "BTC")')
-@click.option("--account", type=str, default="cash", help="cash (default) or margin")
+@click.option("--account", type=click.Choice(['cash', 'margin']), default="cash", help="cash (default) or margin")
+@click.option("--show-all/--no-show-all", default=False, help="show all balances, including empty balances")
 @click.option("--config", type=str, default="config.json", help="path to the config file")
 @click.option("--verbose/--no-verbose", default=False)
-def run(asset, account, config, verbose):
+def run(asset, account, show_all, config, verbose):
 
     btmx_cfg = load_config(get_config_or_default(config))['bitmax']
 
@@ -25,7 +26,7 @@ def run(asset, account, config, verbose):
     ts = utc_timestamp()
     headers = make_auth_headers(ts, "balance", apikey, secret)
     url = f"{host}/{group}/{ROUTE_PREFIX}/{account}/balance"
-    params = dict(asset=asset)
+    params = dict(asset=asset, showAll=show_all)
 
     if verbose:
         print(f"Using url: {url}")
